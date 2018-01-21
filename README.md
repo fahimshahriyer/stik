afc63016865c9679ca98c8eeb0737547f0eea21d
Fahim Shahriyer <shahriyers@gmail.com>
2018-01-22 01:26:21 +0600

    Initial Commit

---
+++ README.md
@@ -0,0 +1,97 @@
+# Stik Boilerplate
+
+This project uses Pug, Sass, Gulp and Browsersync.
+
+Know more about them:
+- [NPM Scripts](https://docs.npmjs.com/misc/scripts)
+- [GulpJS](http://gulpjs.com/)
+- [Pug](https://github.com/pugjs/pug)
+- [Stylus](http://learnboost.github.io/stylus/)
+- [Browsersync](https://www.browsersync.io/)
+
+
+## Getting Started
+
+### Installation
+
+First of all, install the dependencies to run this boilerplate.
+
+- [NodeJS](http://nodejs.org/)
+- [GulpJS](http://gulpjs.com/)
+
+
+```sh
+# Clone this repository
+$ git clone git@github.com:LFeh/kratos-boilerplate.git
+$ cd kratos-boilerplate
+
+# install gulp globally
+$ npm install -g gulp
+
+# install dependencies
+$ npm run setup
+
+```
+
+With the commands above, you have everything to start.
+
+### Folders and Files
+
+```sh
+├── README.md
+├── build
+│   ├── css/
+│   ├── img/
+│   ├── svg/
+│   ├── icons/
+│   ├── js/
+│   ├── index.html
+├── gulpfile.js
+├── package.json
+└── src
+    ├── img/
+    ├── svg/
+│   ├── icons/
+    ├── js/
+    ├── scss
+    │   ├── 0-base/*.scss
+    │   ├── 1-vendors/*.scss
+    │   ├── 2-helpers/*.scss
+    │   ├── 3-layouts/*.scss
+    │   ├── 4-modules/*.scss
+    │   ├── 5-pages/*.scss
+    └── pug
+        └── index.pug
+```
+
+Those folders and file will change during the project.
+
+This project uses [Husky](https://github.com/typicode/husky) to prevent commit and push messy and wrong code.
+
+To help you, this project has a `npm run fix` command to fix all eslint errors.
+
+
+#### Parker CSS
+
+To view a reporter of CSS files, use a `npm run reporter` command.
+
+
+### Tasks
+
+- `npm start`: run all tasks and initialize watch for changes and a server
+- `npm test`: lint javascript and css
+- `npm run setup`: install all dependencies
+- `npm run fix`: command to fix all eslint errors
+- `npm run reporter`: test css complexity
+- `npm run build`: run all tasks to build
+- `npm run html`: compile html files
+- `npm run js`: compile js files
+- `npm run css`: compile css files
+- `npm run images`: compress imaages files
+- `npm run svg`: compress svg files
+- `npom run icons`: generate sprite of icons
+
+
+## License
+
+[MIT License](https://mit-license.org/) © Fahim Shahriyer
---
+++ gulpfile.js
@@ -0,0 +1,164 @@
+'use strict';
+
+const gulp = require('gulp');
+const babel = require('gulp-babel');
+const plumber = require('gulp-plumber');
+const sass = require('gulp-sass');
+const prefixer = require('autoprefixer');
+const gcmq = require('gulp-group-css-media-queries');
+const cssnano = require('gulp-cssnano');
+const sourcemaps = require('gulp-sourcemaps');
+const concat = require('gulp-concat');
+const uglify = require('gulp-uglify');
+const pug = require('gulp-pug');
+const data = require('gulp-data');
+const imagemin = require('gulp-imagemin');
+const browserSync = require('browser-sync');
+const svgmin = require('gulp-svgmin');
+const svgstore = require('gulp-svgstore');
+const cheerio = require('gulp-cheerio');
+const mdcss = require('mdcss');
+const fs = require('fs');
+const del = require('del');
+
+const srcApp = {
+  js: [
+    'src/js/**/*.js'
+  ],
+  css: 'src/scss/**/*.scss',
+  sass: 'src/scss/style.scss',
+  html: 'src/pug/*.pug',
+  icons: 'src/icons/*',
+  svg: 'src/svg/**/*',
+  img: 'src/img/**/*',
+  data: 'src/data/',
+  helpers: 'src/helpers/'
+};
+
+const buildApp = {
+  build: 'build/**/*',
+  js: 'build/js/',
+  css: 'build/css/',
+  html: 'build/',
+  img: 'build/img',
+  svg: 'build/svg',
+  icons: 'build/icons'
+};
+
+let dataJson = {};
+let files = [];
+
+gulp.task('clean', () => {
+  return del(buildApp.build);
+});
+
+gulp.task('css', () => {
+  return gulp.src(srcApp.sass)
+    .pipe(plumber())
+    .pipe(sourcemaps.init())
+    .pipe(sass({
+      compress: false
+    }))
+    .pipe(gcmq())
+    .pipe(cssnano())
+    .pipe(sourcemaps.write('.'))
+    .pipe(gulp.dest(buildApp.css));
+});
+
+gulp.task('js', () => {
+  return gulp.src(srcApp.js)
+    .pipe(plumber())
+    .pipe(sourcemaps.init())
+    .pipe(babel())
+    .pipe(concat('main.js'))
+    .pipe(uglify())
+    .pipe(sourcemaps.write('.'))
+    .pipe(gulp.dest(buildApp.js));
+});
+
+gulp.task('read:data', () => {
+  const dataAdapter = require('./' + srcApp.helpers + 'dataAdapter');
+  fs.readdir(srcApp.data, (err, items) => {
+    files = items.map(item => item.split('.')[0]);
+    items.forEach((file, index) => dataJson[files[index]] = dataAdapter(srcApp.data + file));
+  });
+});
+
+gulp.task('html', () => {
+  return gulp.src(srcApp.html)
+    .pipe(plumber())
+    .pipe(data(dataJson))
+    .pipe(pug())
+    .pipe(gulp.dest(buildApp.html));
+});
+
+gulp.task('images', () => {
+  return gulp.src(srcApp.img)
+    .pipe(plumber())
+    .pipe(imagemin({
+      optimizationLevel: 3,
+      progressive: true,
+      interlaced: true
+    }))
+    .pipe(gulp.dest(buildApp.img));
+});
+
+gulp.task('svg', () => {
+  gulp.src(srcApp.svg)
+    .pipe(svgmin())
+    .pipe(gulp.dest(buildApp.svg));
+});
+
+gulp.task('icons', () => {
+  return gulp.src(srcApp.icons)
+    .pipe(svgmin())
+    .pipe(svgstore({ fileName: 'icons.svg', inlineSvg: true }))
+    .pipe(cheerio({
+      run: function ($, file) {
+        $('svg').addClass('hide');
+        $('[fill]').removeAttr('fill');
+      },
+      parserOptions: { xmlMode: true }
+    }))
+    .pipe(gulp.dest(buildApp.icons));
+});
+
+gulp.task('watch', () => {
+  gulp.watch(srcApp.html, { debounceDelay: 300 }, ['html']);
+  gulp.watch(srcApp.data + '**/*', { debounceDelay: 300 }, ['read:data', 'html']);
+  gulp.watch(srcApp.css, ['css']);
+  gulp.watch(srcApp.js, ['js']);
+  gulp.watch(srcApp.img, ['images']);
+  gulp.watch(srcApp.icons, ['icons']);
+});
+
+gulp.task('browser-sync', () => {
+  var files = [
+    buildApp.build
+  ];
+
+  browserSync.init(files, {
+    server: {
+      baseDir: './build/'
+    },
+  });
+
+});
+
+gulp.task('build', ['clean'], () => {
+  gulp.start(
+    'css',
+    'read:data',
+    'html',
+    'js',
+    'images',
+    'svg',
+    'icons'
+  );
+});
+
+gulp.task('default', [
+  'build',
+  'watch',
+  'browser-sync'
+]);
